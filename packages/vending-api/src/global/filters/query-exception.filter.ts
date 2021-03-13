e0f9b31a286fb997c764b565c91d6203fb2b8480
Siripong <aeio.yu@gmail.com>
2021-03-13 20:36:44 +0700

    refactor: change exception type of query failed filter from httpException to QueryFailedError

--- packages/vending-api/src/global/filters/query-exception.filter.ts
+++ packages/vending-api/src/global/filters/query-exception.filter.ts
@@ -1,20 +1,20 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
 import { Response } from 'express';
 import { QueryFailedError } from 'typeorm';
 
 @Catch(QueryFailedError)
 export class QueryExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: QueryFailedError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     // const request = ctx.getRequest<Request>();
+
     const castException = exception as any;
     const message = castException.detail;
 
     response.status(400).json({
       statusCode: 400,
       message: message,
-      // timestamp: new Date().toISOString(),
     });
   }
 }
