624c01de5323c89d4754b21f4dc277c750b785df
Siripong <aeio.yu@gmail.com>
2021-03-13 20:37:46 +0700

    feat: add name,latitude, longitude to machine entity to support location mapping for google map

--- packages/vending-api/src/machines/machines.entity.ts
+++ packages/vending-api/src/machines/machines.entity.ts
@@ -6,11 +6,20 @@ export class Machine extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ default: 'Vending Machine' })
+  name: string;
+
   @Column({ unique: true })
   serialNo: string;
 
   @Column()
   location: string;
+
+  @Column({ nullable: true })
+  latitude: string;
+
+  @Column({ nullable: true })
+  longitude: string;
 
   @Column({ default: true })
   active: boolean;
