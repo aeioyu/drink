aba2a6dd486c29d80d720e5db4e01fb89a78a0d5
Siripong <aeio.yu@gmail.com>
2021-03-15 02:43:07 +0700

    feat: enable cors

--- packages/vending-api/src/main.ts
+++ packages/vending-api/src/main.ts
@@ -5,6 +5,7 @@ import { QueryExceptionFilter } from './global/filters/query-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new QueryExceptionFilter());
+  app.enableCors();
   await app.listen(8001);
 }
 bootstrap();
